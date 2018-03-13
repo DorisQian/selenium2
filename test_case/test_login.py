823d927aac3109ead0d9269d3dcd17b921241155
DorisQian <qianyh@163.com>
2018-03-13 17:57:28 +0800

    modified test_login

--- test_case/test_login.py
+++ test_case/test_login.py
@@ -14,64 +14,107 @@ __author__ = 'Doris Qian'
 
 
 class TestLogin(unittest.TestCase):
-
     u"""测试登录模块"""
 
     logger = Logger('INFO')
     path = os.path.abspath('..') + os.sep + 'conf' + os.sep
     data_file = path + 'data.csv'
-    # with open(data_file, 'rb') as f:
-        # data = csv.reader(f)
-    data = csv.reader(open(data_file, 'r'))
+    username, password = [], []
     logger.info('read csv file')
+    with open(data_file, 'r') as f:
+        data = csv.reader(f)
+        for i, info in enumerate(data):
+            username.append(info[0])
+            password.append(info[1])
+    logger.info('username list: %s' % username)
+    logger.info('password list: %s' % password)
+
+    # data = csv.reader(open(data_file, 'r'))
 
     def setUp(self):
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(30)
-        # self.base_url = "https://mail.sina.com.cn/"
-        # self.driver.get(self.base_url)
+        self.login = LoginPage(self.driver)
+        self.login.open()
+
+    def test_null_pwd(self):
+
+        u"""密码为空"""
+
+        self.logger.info('username: %s, password: %s' % (self.username[1], self.password[1]))
+        try:
+            self.login.type_username(self.username[1])
+            self.login.type_password(self.password[1])
+            self.login.login()
+            self.driver.implicitly_wait(10)
+            time.sleep(1)
+            text = self.login.find_element(By.XPATH,
+                                           "/html/body/div[1]/div/div[2]/div/div/div[4]/div[1]/div[1]/div[1]/span[2]").text
+            self.assertEquals(text, u"请输入密码")
+        except Exception as msg:
+            self.logger.warning("reasons of exception %s" % msg)
+            now = time.strftime('%Y-%m-%d_%H-%M-%S')
+            self.driver.get_screenshot_as_file('../images/test_pwd_null_%s.png' % now)
+            raise
+
+    def test_null_username(self):
+
+        u""""用户名为空"""
+
+        self.logger.info('username: %s, password: %s' % (self.username[2], self.password[2]))
+        try:
+            self.login.type_username(self.username[2])
+            self.login.type_password(self.password[2])
+            self.login.login()
+            self.driver.implicitly_wait(10)
+            text = self.login.find_element(By.XPATH,
+                                           "/html/body/div[1]/div/div[2]/div/div/div[4]/div[1]/div[1]/div[1]/span[1]").text
+            self.assertEqual(text, u"请输入邮箱名")
+        except Exception as msg:
+            self.logger.warning("reasons of exception %s" % msg)
+            now = time.strftime('%Y-%m-%d_%H-%M-%S')
+            self.driver.get_screenshot_as_file('../images/test_null_username_%s.png' % now)
+            raise
+
+    def test_wrong_pwd(self):
+
+        u"""密码错误"""
+
+        self.logger.info('username: %s, password: %s' % (self.username[3], self.password[3]))
+        try:
+            self.login.type_username(self.username[3])
+            self.login.type_password(self.password[3])
+            self.login.login()
+            self.driver.implicitly_wait(10)
+            text = self.login.find_element(By.XPATH,
+                                           "/html/body/div[1]/div/div[2]/div/div/div[4]/div[1]/div[1]/div[1]/span[1]").text
+            self.assertEqual(text, u"登录名或密码错误")
+        except Exception as msg:
+            self.logger.warning("reasons of exception %s" % msg)
+            now = time.strftime('%Y-%m-%d_%H-%M-%S')
+            self.driver.get_screenshot_as_file('../images/test_wrong_pwd_%s.png' % now)
+            raise
 
     def test_right(self):
 
         u"""成功登陆"""
 
-        for i, info in enumerate(self.data):
-            if i == 0:
-                print(info)
-                username = info[0]
-                password = info[1]
-                self.logger.info('username:%s, password: %s' % (username, password))
+        self.logger.info('username:%s, password: %s' % (self.username[0], self.password[0]))
         try:
-            login = LoginPage(self.driver)
-            login.open()
-            login.type_username(username)
-            login.type_password(password)
-            login.login()
+            self.login.type_username(self.username[0])
+            self.login.type_password(self.password[0])
+            self.login.login()
             self.driver.implicitly_wait(10)
             time.sleep(1)
-            text = login.find_element(By.XPATH, "/html/body/div[1]/div/div[4]/div[1]/div[3]/div[1]/span/em[2]").text
-            self.assertEqual(text, username)
+            text = self.login.find_element(By.XPATH,
+                                           "/html/body/div[1]/div/div[4]/div[1]/div[3]/div[1]/span/em[2]").text
+            self.assertEqual(text, self.username[0])
         except Exception as msg:
             self.logger.warning("reasons of exception %s" % msg)
-            now = time.strftime("%Y-%m-%d_%H:%M:%S")
-            self.driver.get_screenshot_as_file('../images/testlogin_%s.png' % now)
-            self.logger.info('generation screeshot-testlogin_%s.png' % now)
+            now = time.strftime("%Y-%m-%d_%H-%M-%S")
+            self.driver.get_screenshot_as_file('../images/test_right_%s.png' % now)
+            self.logger.info('generation scree shot-test_right_%s.png' % now)
             raise
 
-    def test_pwdnull(self):
-        for i, info in enumerate(self.data):
-            if i == 1:
-                username = info[0]
-                password = info[1]
-                self.logger.info('username: %s, password: %s' % username, password)
-        try:
-            login = LoginPage(self.driver)
-            login.open()
-            login.type_username(username)
-            login.type_password(password)
-            login.login()
-
-
-    def setDown(self):
-        self.data.close()
+    def tearDown(self):
         self.driver.quit()
