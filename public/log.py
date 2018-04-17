58aeeaebb2e25681bd14bd4ef3cec6e647c5f073
DorisQian <qianyh@163.com>
2018-04-17 17:27:34 +0800

    修改日志封装

--- public/log.py
+++ public/log.py
@@ -1,75 +1,42 @@
 # !/usr/bin/env python3
-# -*- coding = utf-8 -*-
+# -*- coding=utf-8 -*-
 
 import logging
-import logging.handlers
 import time
 import os
-import sys
+import re
+from logging import handlers
 
-__author__ = 'Doris Qian'
 
-
-class Logger():
+def log(name):
+    U"""
+    封装logger
+    :return:logging,供外部使用
     """
-    日志定义
-    """
-    def __init__(self, log_level):
-        self.log_level = log_level
+    logger = logging.getLogger(name)
+    logger.setLevel('INFO')
 
-    def print_log(self, level, message):
-        # myapp的初始化工作
-        logger = logging.getLogger(os.path.basename(sys.argv[0]))
-        logger.setLevel(self.log_level)
+    path = os.getcwd().split('sele')[0] + 'sele' + os.sep + 'logs' + os.sep
+    file = path + 'ui'
 
-        ch = logging.StreamHandler()
-        ch.setLevel(self.log_level)
-        # 添加TimedRotatingFileHandler
-        # 定义一个1天换一次log文件的handler
-        # 保留5个旧log文件
-        day = time.strftime("%Y-%m-%d")
-        filename = "../logs/testing" + day + ".log"
-        fh = logging.handlers.TimedRotatingFileHandler(filename, when='D', interval=1, backupCount=5)
-        fh.setLevel(logging.INFO)
+    ch = logging.StreamHandler()
+    # ch.setLevel(self.level)
 
-        # 设置后缀名称，跟strftime的格式一样
-        fh.suffix = "%Y-%m-%d.log"
+    # save five days log(backupcount)
+    fh = handlers.TimedRotatingFileHandler(file, when='D', interval=1, backupCount=5, encoding='utf8')
+    # fh.setLevel(self.level)
+    fh.suffix = '%Y-%m-%d.log'
 
-        log_format = logging.Formatter('%(asctime)s %(levelname)s %(message)s [%(filename)s:%(lineno)d]')
+    log_format = logging.Formatter('%(asctime)s %(levelname)s [%(filename)s:%(lineno)d]  %(message)s')
+    ch.setFormatter(log_format)
+    fh.setFormatter(log_format)
 
-        ch.setFormatter(log_format)
-        fh.setFormatter(log_format)
+    logger.addHandler(ch)
+    logger.addHandler(fh)
+    return logger
 
-        logger.addHandler(ch)
-        logger.addHandler(fh)
-
-        if level == 'info':
-            logger.info(message)
-        elif level == 'debug':
-            logger.debug(message)
-        elif level == 'warning':
-            logger.warning(message)
-        elif level == 'error':
-            logger.error(message)
-        logger.removeHandler(ch)
-        logger.removeHandler(fh)
-
-    def debug(self, message):
-        self.print_log('info', message)
-
-    def info(self, message):
-        self.print_log('info', message)
-
-    def warning(self, message):
-        self.print_log('warning', message)
-
-    def error(self, message):
-        self.print_log('error', message)
-'''
 if __name__ == '__main__':
-    log = Logger('INFO')
-    log.info('info msg1000')
-    log.debug('debug msg')
-    log.warning('warning msg')
-    log.error('error msg')
-'''
+
+    logging = log('test')
+    logging.info('test')
+
