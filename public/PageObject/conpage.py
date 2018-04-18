7df02992d4fe1f3433031ad7cc81dcf3d212866d
DorisQian <qianyh@163.com>
2018-04-18 17:56:14 +0800

    添加公共login方法，conpage页面

---
+++ public/PageObject/conpage.py
@@ -0,0 +1,21 @@
+# !/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
+__author__ = 'Doris Qian'
+
+from public.PageObject.page import Page
+from selenium.webdriver.common.by import By
+
+
+class ConPage(Page):
+	u"""系统配置页面封装"""
+
+	def sys_conf(self):
+		self.find_element(By.LINK_TEXT, u'系统配置').click()
+
+	def function_conf(self):
+		self.find_element(By.LINK_TEXT, u'功能配置').click()
+
+	def manufacturer(self):
+		self.find_element(By.LINK_TEXT, u' 厂商管理').click()
+
--- public/login.py
+++ public/login.py
@@ -3,39 +3,24 @@
 
 __author__ = 'Doris Qian'
 
-from public.PageObject.loginPage import LoginPage
-from selenium import webdriver
-import time
-from log import Logger
-import logging
-
-log = Logger('INFO')
+from public.PageObject.loginpage import LoginPage
+from public.log import log
+import os
 
 
-class Login():
+class Login:
     """
-    登录邮箱的公共类
+    登录soc的公共类
     """
-    log.info("login the sina mail...")
+    def __init__(self):
+        self.logger = log(os.path.basename(__file__))
+        self.browser = LoginPage()
 
-    @staticmethod
-    def login(driver, username, password):
-        login_page = LoginPage(driver)
-        login_page.open()
-        login_page.type_username(username)
-        login_page.type_password(password)
-        login_page.login()
-        time.sleep(2)
-        assert (username == 'doris_test@sina.com'), u"用户名称不匹配，登录失败!"
-
-
-def main():
-    username = 'doris_test@sina.com'
-    password = 'admin@123'
-    driver = webdriver.Chrome()
-    # url = '\\'
-    Login().login(driver, username, password)
-
-
-if __name__ == '__main__':
-    main()
+    def login(self):
+        self.browser.open()
+        self.browser.type_username('cfgadmin')
+        self.browser.type_password('password')
+        self.browser.type_security('abcd')
+        self.browser.login()
+        self.browser.kick_user()
+        self.logger.info('login soc')
