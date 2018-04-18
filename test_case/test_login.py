0343f2a54b91d0a5d57e9553a7d5c7e324ec3a2e
DorisQian <qianyh@163.com>
2018-04-18 14:21:41 +0800

    完成登录用例

--- test_case/test_login.py
+++ test_case/test_login.py
@@ -7,6 +7,8 @@ from public.log import log
 from public.PageObject.loginpage import LoginPage
 from selenium.webdriver.common.by import By
 from public.dataConnect import Database
+from conf.configure import conf
+from suds.client import Client
 import unittest
 import xlrd
 import time
@@ -92,7 +94,7 @@ class TestLogin(unittest.TestCase):
 			self.browser.login()
 			time.sleep(2)
 			text = self.driver.find_element(By.CLASS_NAME, 'warn').text
-			self.assertEqual(text, u'密码不可空')
+			self.assertEqual(text, u'验证码不可空')
 		except Exception as msg:
 			self.logger.warning("assert failed: %s" % msg)
 			self.driver.get_screenshot_as_file(
@@ -121,22 +123,26 @@ class TestLogin(unittest.TestCase):
 		minute = self.data.select('net_sysconfig', ['DATA'], {'NAME': 'manag_safty_UserLockedTimes'})
 		self.browser.type_username(self.username[7])
 		self.browser.type_password(self.password[7])
-		self.browser.type_security(self.security[7])
 		self.logger.info(
 			'username, password, security: %s, %s, %s' % (self.username[7], self.password[7], self.security[7]))
-		for t in range(lock_time + 1, -1):
+		for t in range(int(lock_time), 0, -1):
 			try:
+				self.browser.type_security(self.security[7])
 				self.browser.login()
 				time.sleep(2)
 				text = self.driver.find_element(By.CLASS_NAME, 'warn').text
-				if t == 0:
+				if t == 1:
 					self.assertEqual(text, u'当前用户已被锁定，请%s分钟后重试！' % minute)
-				self.assertEqual(text, u'密码错误!您还有%s次机会。' % (t - 1))
+				else:
+					self.assertEqual(text, u'密码错误!您还有%s次机会。' % (t - 1))
 			except Exception as msg:
 				self.logger.warning("assert failed: %s" % msg)
 				self.driver.get_screenshot_as_file(
 					'../images/test_user_lock_%s.png' % self.now)
-		self.data.update('uum_user', {'STATE': 0}, {'login_name': 'cfgadmin'})
+		service_url = conf['service_url'] + 'UUMSystemService?wsdl'
+		client = Client(service_url)
+		param = {'userId': 19, 'state': 0}
+		client.service.uumUserStateUpdate(**param)
 
 	def test_z_success_login(self):
 		u"""登录成功"""
@@ -149,11 +155,22 @@ class TestLogin(unittest.TestCase):
 			self.browser.login()
 			time.sleep(2)
 			text = self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/ul[1]/li[1]/a/span").text
-			self.assertEqual(text, u'态势感知')
+			self.assertEqual(text, u'安全监控')
 		except Exception as msg:
 			self.logger.warning("assert failed: %s" % msg)
 			self.driver.get_screenshot_as_file(
 				'../images/test_z_success_login_%s.png' % self.now)
+
+	def test_z_success_logout(self):
+		u"""成功登出"""
+		try:
+			self.browser.find_element(By.XPATH, '//*[@id="navbar"]/ul[2]/li[5]/img').click()
+			text = self.driver.find_element(By.CLASS_NAME, "login-tooltip").text
+			self.assertEqual(text, u'欢迎登录')
+		except Exception as msg:
+			self.logger.warning("assert failed: %s" % msg)
+			self.driver.get_screenshot_as_file(
+				'../images/test_z_success_logout_%s.png' % self.now)
 
 	@classmethod
 	def tearDownClass(cls):
